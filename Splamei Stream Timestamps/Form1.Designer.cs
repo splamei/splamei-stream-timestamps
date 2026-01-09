69b3514399a9ec5a1e4157cae3c2063955f3fa61
splamei <129879626+splamei@users.noreply.github.com>
2026-01-09 16:31:10 +0000

    Added better names for UI items

--- Splamei Stream Timestamps/Form1.Designer.cs
+++ Splamei Stream Timestamps/Form1.Designer.cs
@@ -29,138 +29,138 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
+            this.delayNum = new System.Windows.Forms.NumericUpDown();
+            this.delayTxt = new System.Windows.Forms.Label();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.button5 = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.pauseBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.timeDisplay = new System.Windows.Forms.ListView();
+            this.recordTime = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.statusTxt = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.delayNum)).BeginInit();
             this.SuspendLayout();
             // 
-            // numericUpDown1
+            // delayNum
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(165, 11);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(262, 20);
-            this.numericUpDown1.TabIndex = 0;
+            this.delayNum.Location = new System.Drawing.Point(165, 11);
+            this.delayNum.Name = "delayNum";
+            this.delayNum.Size = new System.Drawing.Size(262, 20);
+            this.delayNum.TabIndex = 0;
             // 
-            // label1
+            // delayTxt
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Stream delay offset (seconds)";
-            this.toolTip1.SetToolTip(this.label1, "The time it takes for your stream to be displayed to views (It\'s often 8 seconds " +
+            this.delayTxt.AutoSize = true;
+            this.delayTxt.Location = new System.Drawing.Point(13, 13);
+            this.delayTxt.Name = "delayTxt";
+            this.delayTxt.Size = new System.Drawing.Size(146, 13);
+            this.delayTxt.TabIndex = 1;
+            this.delayTxt.Text = "Stream delay offset (seconds)";
+            this.toolTip1.SetToolTip(this.delayTxt, "The time it takes for your stream to be displayed to views (It\'s often 8 seconds " +
         "on normal latency)");
             // 
-            // button1
+            // startBtn
             // 
-            this.button1.Location = new System.Drawing.Point(16, 53);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
+            this.startBtn.Location = new System.Drawing.Point(16, 53);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 2;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // pauseBtn
             // 
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(98, 53);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Pause";
-            this.button2.UseVisualStyleBackColor = true;
+            this.pauseBtn.Enabled = false;
+            this.pauseBtn.Location = new System.Drawing.Point(98, 53);
+            this.pauseBtn.Name = "pauseBtn";
+            this.pauseBtn.Size = new System.Drawing.Size(75, 23);
+            this.pauseBtn.TabIndex = 3;
+            this.pauseBtn.Text = "Pause";
+            this.pauseBtn.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // stopBtn
             // 
-            this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(179, 53);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Stop";
-            this.button3.UseVisualStyleBackColor = true;
+            this.stopBtn.Enabled = false;
+            this.stopBtn.Location = new System.Drawing.Point(179, 53);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 4;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // clearBtn
             // 
-            this.button4.Enabled = false;
-            this.button4.Location = new System.Drawing.Point(260, 53);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Clear";
-            this.button4.UseVisualStyleBackColor = true;
+            this.clearBtn.Enabled = false;
+            this.clearBtn.Location = new System.Drawing.Point(260, 53);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 5;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
             // 
-            // listView1
+            // timeDisplay
             // 
-            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.timeDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 102);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(560, 296);
-            this.listView1.TabIndex = 6;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.timeDisplay.HideSelection = false;
+            this.timeDisplay.Location = new System.Drawing.Point(12, 102);
+            this.timeDisplay.Name = "timeDisplay";
+            this.timeDisplay.Size = new System.Drawing.Size(560, 296);
+            this.timeDisplay.TabIndex = 6;
+            this.timeDisplay.UseCompatibleStateImageBehavior = false;
             // 
-            // button5
+            // recordTime
             // 
-            this.button5.Enabled = false;
-            this.button5.Location = new System.Drawing.Point(342, 53);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(85, 23);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "Record Time";
-            this.button5.UseVisualStyleBackColor = true;
+            this.recordTime.Enabled = false;
+            this.recordTime.Location = new System.Drawing.Point(342, 53);
+            this.recordTime.Name = "recordTime";
+            this.recordTime.Size = new System.Drawing.Size(85, 23);
+            this.recordTime.TabIndex = 7;
+            this.recordTime.Text = "Record Time";
+            this.recordTime.UseVisualStyleBackColor = true;
             // 
-            // progressBar1
+            // progressBar
             // 
-            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.progressBar1.Location = new System.Drawing.Point(13, 416);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 13);
-            this.progressBar1.TabIndex = 8;
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.progressBar.Location = new System.Drawing.Point(13, 416);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 13);
+            this.progressBar.TabIndex = 8;
             // 
-            // label2
+            // statusTxt
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(119, 416);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Not recording. No data";
+            this.statusTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.statusTxt.AutoSize = true;
+            this.statusTxt.Location = new System.Drawing.Point(119, 416);
+            this.statusTxt.Name = "statusTxt";
+            this.statusTxt.Size = new System.Drawing.Size(115, 13);
+            this.statusTxt.TabIndex = 9;
+            this.statusTxt.Text = "Not recording. No data";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 441);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.statusTxt);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.recordTime);
+            this.Controls.Add(this.timeDisplay);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.pauseBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.delayTxt);
+            this.Controls.Add(this.delayNum);
             this.MinimumSize = new System.Drawing.Size(457, 320);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Splamei Stream Timestamps";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.delayNum)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,17 +168,17 @@
 
         #endregion
 
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown delayNum;
+        private System.Windows.Forms.Label delayTxt;
         private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button pauseBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.ListView timeDisplay;
+        private System.Windows.Forms.Button recordTime;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label statusTxt;
     }
 }
 
